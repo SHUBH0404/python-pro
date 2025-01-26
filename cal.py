9a9c714655285e49788fe4e2c7aea6a28028a7e7
SHUBH0404 <shristishiv667@gmail.com>
2025-01-26 18:58:25 +0530

    Added 2 files.

---
+++ cal.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+
+#title & icon Adjustment
+root = Tk()
+root.title("Shubh Calculator")
+root.geometry("450x550")
+root.configure(bg="black")
+root.resizable(False, False)
+root.iconphoto(True, PhotoImage(file="images/calculator.png"))
+
+#layout of Calculator
+
+
+
+#loop the window
+root.mainloop()
